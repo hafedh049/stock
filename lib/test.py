ed0bd3846cef1b03a42809cbec74324c67f6a706
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-17 21:32:11 +0100

    A

--- lib/test.py
+++ lib/test.py
@@ -1,32 +1,31 @@
 from faker import Faker
 from json import dump
 from string import ascii_letters
-from random import sample,randint
+from random import sample, randint
 
 fake = Faker()
 
 l = []
 
-with open() as fp:
+with open("test.json", "w") as fp:
     for _ in range(100):  # Generating 10 fake products
-        
+        print(_)
         product_id = ""
 
         while True:
-            product_id = "".join(["#",*sample(ascii_letters,5)])
-            if not product_id in [item["product_key"] for item in l]:
+            product_id = "".join(["#", *sample(ascii_letters, 5)]).upper()
+            if not product_id in [item["product_id"] for item in l]:
                 break
 
         product_name = ""
-        
+
         while True:
-            product_name = fake.word()
+            product_name = fake.word().title()
             if not product_name in [item["product_name"] for item in l]:
                 break
-        
+
         product_description = ""
 
-        
         while True:
             product_description = fake.text()
             if not product_description in [item["product_description"] for item in l]:
@@ -34,18 +33,18 @@ with open() as fp:
 
         product_price = 0
         product_sell_price = 0
-        
+
         while True:
-            product_price = fake.random_number(digits=4)
-            product_sell_price = fake.random_number(digits=4)
+            product_price = randint(1, 100)
+            product_sell_price = randint(1, 100)
             if product_sell_price > product_price:
-                break        
+                break
 
         product_picture = []
 
-        OFSNotification = randint(30,60)
+        OFSNotification = randint(30, 60)
 
-        product_qunatity = randint(65,380)
+        product_qunatity = randint(65, 380)
 
         discount = 0
 
@@ -54,32 +53,35 @@ with open() as fp:
         entry_date = "01/09/2023"
 
         product_category = ""
-        
+
         while True:
             product_category = fake.name()
-            if product_category in [item["product_category"] for item in l]:
-                break    
+            if not product_category in [item["product_category"] for item in l]:
+                break
 
         product_brand = ""
-        
+
         while True:
             product_brand = fake.name()
-            if product_brand in [item["product_brand"] for item in l]:
-                break        
+            if not product_brand in [item["product_brand"] for item in l]:
+                break
 
-        l.append({"product_id":product_id,
-                  "product_name":product_name,
-                  "product_description":product_description,
-                  "product_price":product_price,
-                  "product_sell_price":product_sell_price,
-                  "product_picture":product_picture,
-                  "soeuil":OFSNotification,
-                  "product_qunatity":product_qunatity,
-                  "product_discount":discount,
-                  "product_status":status,
-                  "product_date":entry_date,
-                  "product_category":product_category,
-                  "product_brand":product_brand,
-                  })
+        l.append(
+            {
+                "product_id": product_id,
+                "product_name": product_name,
+                "product_description": product_description,
+                "product_price": product_price,
+                "product_sell_price": product_sell_price,
+                "product_picture": product_picture,
+                "seuil": OFSNotification,
+                "product_qunatity": product_qunatity,
+                "product_discount": discount,
+                "product_status": status,
+                "product_date": entry_date,
+                "product_category": product_category,
+                "product_brand": product_brand,
+            }
+        )
 
-    dump(l,fp)
+    dump(l, fp)
